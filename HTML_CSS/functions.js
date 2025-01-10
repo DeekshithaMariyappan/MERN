37cb34955408a6fb18154955370d22f5c65f9f86
Deekshitha <deekshithamariyappan@gmail.com>
2025-01-10 22:04:53 +0530

    Adding the modified files

--- HTML_CSS/functions.js
+++ HTML_CSS/functions.js
@@ -1,6 +1,25 @@
-//with retuen and with parameter
+//with return and with parameter
 function SECE(dept){
     console.log("The department is",dept)
     return "Section of the department is A"
 }
 console.log(SECE("CSE"));
+
+//without parameter without return
+function department(){
+    console.log("Department CSE")
+}
+department();
+
+//with paramater without return
+function print(name){
+    console.log("Name: " + name)
+}
+print("Deekshitha");
+
+//without parameter with return 
+function printMsg(){
+    return "I'm Deekshitha"
+}
+console.log(printMsg());
+
