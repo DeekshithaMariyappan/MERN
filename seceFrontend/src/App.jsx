379a2c8ce461e4d647f61b8726efa3f34f77c485
Deekshitha <deekshithamariyappan@gmail.com>
2025-01-21 14:57:48 +0530

    Adding router files

--- seceFrontend/src/App.jsx
+++ seceFrontend/src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import About from './components/FunctionalComponents/About';
-import ClassCompEG from './components/classComponents/ClassCompEG';
 import Gallery from './components/FunctionalComponents/Gallery';
 import Home from './components/FunctionalComponents/Home';
 import Contact from './components/FunctionalComponents/Contact';
 import Navbar from './components/FunctionalComponents/Navbar';
+import Signup from './components/FunctionalComponents/Signup';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -18,6 +18,7 @@ function App() {
       <Route path="/about"element={<About />}></Route>
       <Route path="/gallery"element={<Gallery page="Gallery" image="SECE Logo" />}></Route>
       <Route path="/contact"element={<Contact />}></Route>
+      <Route path="/signup"element={<Signup />}></Route>
       </Routes>
     </BrowserRouter>
   )
--- seceFrontend/src/components/FunctionalComponents/Navbar.jsx
+++ seceFrontend/src/components/FunctionalComponents/Navbar.jsx
@@ -1,13 +1,16 @@
 import'../../css/Navbar.css';
+import { Link } from 'react-router-dom';
+
 const Navbar=()=>{
     return(
         <header>
         <nav>
             <ol>
-                <li>Home</li>
-                <li>About</li>
-                <li>Gallery</li>
-                <li>Contact</li>
+                <li><Link to='/' className="link">Home</Link></li>
+                <li><Link to='/about' className="link">About</Link></li>
+                <li><Link to='/gallery' className="link">Gallery</Link></li>
+                <li><Link to='/contact' className="link">Contact</Link></li>
+                <li><Link to='/signup 'className="link">SignUp</Link></li>
             </ol>
         </nav>
         </header>
